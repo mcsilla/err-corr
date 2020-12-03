9bcea630e60f966dcd65430a926c8c44d5fca8e3
mcsilla <majoroscsilla88@gmail.com>
2020-12-03 14:10:13 +0100

    Check max length of a tokenized sentence

--- create_corr_data.py
+++ create_corr_data.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 from pathlib import Path
+from transformers import BertTokenizerFast
+import numpy as np
+import tensorflow as tf
+import random
 
 BASE_DIR = Path(__file__).parent
 LANGUAGE = "ro"
@@ -13,6 +17,12 @@ def read_files(language = LANGUAGE):
            yield f.readlines()
 
 if __name__ == "__main__":
+    character_tokenizer = BertTokenizerFast(BASE_DIR / "data/tokenizer/alphabet", do_lower_case=False)
+    max_line_lengths = []
     for lines in read_files():
-        print(lines[:10])
+        # max_index = np.argmax([len(character_tokenizer.tokenize(line)) for line in lines])
+        max_line_lengths.append(max([len(character_tokenizer.tokenize(line)) for line in lines]))
+        print(max_line_lengths)
         break
+    max_line_length = max(max_line_lengths)
+    print(max_line_length)
